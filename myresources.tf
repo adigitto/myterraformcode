a9487a7469ccbba8580751175316176a106505c0
Aditya Kulkarni <aditya.a.kulkarni@oracle.com>
2020-08-18 20:26:18 -0400

    Adding EIP association

--- myresources.tf
+++ myresources.tf
@@ -1,15 +1,21 @@
+# -------- Provider block ---------#
 provider "aws" {
   profile = "default"
   region  = "us-east-1" 
 }
 
+# -------- S3 bucket ---------#
 resource "aws_s3_bucket" "mytfbucket-prod" {
   bucket = "adi-tf-08102020"
   acl    = "private"
 }
 
+
+# ------- AWS Default VPC --------# 
 resource "aws_default_vpc" "my_default_vpc" {}
 
+
+# --------- Security Group--------#
 resource "aws_security_group" "tf_web_security" {
   name        = "tf-security-webservers"
   description = "Terraform learning test security group"
@@ -28,6 +34,7 @@ resource "aws_security_group" "tf_web_security" {
     protocol   = "tcp"
     cidr_blocks = ["192.168.1.64/32"]
   }
+
   #Define the egress rules block
   egress {
     from_port = 0
@@ -40,6 +47,35 @@ resource "aws_security_group" "tf_web_security" {
   }
 }
 
+# -------- EC2 Instance ---------- #
+resource "aws_instance" "web-instance" {
+  count = 2
 
+  ami = "ami-xyz"  # Need to get exact ami-ID
+  instance_type = "t2.nano"
+
+  vpc_security_group_ids = [
+    aws_security_group.tf_web_security.id
+  ]
+
+  tags = {
+    "Terraform" : "True"
+  }
+}
+
+
+# ------- Elastic IP ------------- #
+
+resource "aws_eip_association" "eip_allocation" {
+  instance_id   = aws_instance.web-instance.0.id
+  allocation_id = aws_eip.my_instance_eip.id
+}
+
+resource "aws_eip" "my_instance_eip" {
+  tags = {
+    "Terraform" : "true"
+  }
+
+}
 
  
